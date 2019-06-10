c63b1f3d2d4d73b9f51f758b38f92ef571e02c7c
Michael Lekrans <mlekrans@hotmail.com>
2019-06-10 05:58:02 +0200

    bind

---
+++ projects/concepts/functions_and_scope/code/bind/app.js
@@ -0,0 +1,25 @@
+const log = require('lekrans_code_helper');
+
+const appName = 'bind';
+
+log.header(appName);
+
+log.subHeader1(
+  'Sometimes we need to make a COPY of the function AND change the THIS value',
+);
+
+log.subHeader2(
+  'We make a copy of getId and BIND newCar to it so it will ALWAYS use that context',
+);
+
+const o = {
+  carId: 123,
+  getId() {
+    return this.carId;
+  },
+};
+
+const newCar = { carId: 456 };
+const newFn = o.getId.bind(newCar);
+log.log(newFn());
+log.footer(appName);
